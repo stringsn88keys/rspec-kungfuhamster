f4d4eaddaada6213370b820df9e7a0680e531b01
Thomas Powell <twilliampowell@gmail.com>
2014-11-18 14:47:05 -0500

    Colorized kung fu hamster, started filling out README

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ rspec-kungfuhamster
 ======================
 
 Animated kung fu hamster formatter
+
+# Installation
+
+Add this to you Gemfile (in the test group) and run `bundle install`:
+
+``` ruby
+gem 'rspec-kungfuhamster'
+```
--- lib/rspec_kung_fu_hamster.rb
+++ lib/rspec_kung_fu_hamster.rb
@@ -1,5 +1,5 @@
 require 'rspec'
- require 'rspec/core/formatters/base_text_formatter'
+require 'rspec/core/formatters/base_text_formatter'
 
 rspec_bin = $0.split('/').last
 
@@ -8,21 +8,55 @@ class RspecKungFuHamster < RSpec::Core::Formatters::BaseTextFormatter
 #    :start,
 #     :example_start
 #    :message,
-    :example_passed
-#    :example_pending,
-#    :example_failed,
+    :example_passed,
+    :example_pending,
+    :example_failed
 #    :dump_failures
 
   def initialize(output)
     @index = 0
+    @example_passed = 0
+    @example_pending = 0
+    @example_failed = 0
     super(output)
   end
 
+  def color_positions(length)
+    total = @example_passed + @example_pending + @example_failed
+    passed_percent = @example_passed * length / total
+    pending_percent = @example_pending * length / total
+
+    [passed_percent, passed_percent + pending_percent, length]
+  end
+
+  GREEN = "\e[32m"
+  YELLOW = "\e[33m"
+  RED = "\e[31m"
+  RESET = "\e[0m"
+
+  def colorize(string)
+    green, yellow, red = color_positions(string.length)
+    GREEN + string[0...green] + YELLOW + string[green...yellow] + RED + string[yellow...red] + RESET
+  end
+
+  def display(strings)
+    colorize(strings.join("\n"))
+  end
+
+  def output_hamster
+    output.puts "\e[2J\e[;H" + display(hamster_and_next)
+  end
   def example_passed(notification)
-    output.puts "\e[2J\e[;H" << hamster_and_next.join("\n")
+    @example_passed += 1
+    output_hamster
+  end
+  def example_pending(notification)
+    @example_pending += 1
+    output_hamster
   end
   def example_failed(notification)
-    output.puts "\e[2J\e[;H" << hamster_and_next.join("\n")
+    @example_failed += 1
+    output_hamster
   end
 
   def hamster_and_next
